ee146dc80753379a066244a5a6b3e8d80d6b93b7
Brett Kuhn <brettkuhn87@gmail.com>
2022-09-22 10:26:58 -0600

    Add function to load classic game view, create HTML elements for fighters, updated cursor pointer CSS

--- main.js
+++ main.js
@@ -5,16 +5,27 @@ var computerPlayer;
 
 //HTML elements
 var homePage = document.querySelector('#homePage');
-var classicGameButton = document.querySelector('#classicGameButton')
-var spookyGameButton = document.querySelector('#spookyGameButton')
+var classicGameButton = document.querySelector('#classicGameButton');
+var spookyGameButton = document.querySelector('#spookyGameButton');
+
+var classicGamePage = document.querySelector('#classicGamePage');
+var classicRock = document.querySelector('#classicRock');
+var classicPaper = document.querySelector('#classicPaper');
+var classicScissors = document.querySelector('#classicScissors');
 
 //Event Listeners
 window.addEventListener('load', createGame);
-
+classicGameButton.addEventListener('click', loadClassicGame)
 
 //Functions/Event Handlers
 function createGame() {
   humanPlayer = new Player('human');
   computerPlayer = new Player('computer');
   currentGame = new Game();
+}
+
+function loadClassicGame() {
+  classicGamePage.classList.remove('hidden');
+  homePage.classList.add('hidden');
+  currentGame.type = 'classic';
 }
