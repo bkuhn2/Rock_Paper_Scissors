6990f2189bfef5e6e8cfd9c9ea57e818a4667532
Brett Kuhn <brettkuhn87@gmail.com>
2022-09-23 09:43:22 -0600

    Refactor selectFighters and takeTurn functions to make more dry, concise

--- main.js
+++ main.js
@@ -93,18 +93,13 @@ function loadSpookyGame() {
 
 function selectFighters(event) { //make this FOR BOTH CLASSIC AND SPOOKY
   event.preventDefault();
+
   if (allFighterTypes.includes(event.target.id)) {
-    humanPlayer.takeTurn(event);
-    currentGame.humanFighter = humanPlayer.fighter; //necessary? happen in take takeTurn fxn with current game as 2nd param?
+    humanPlayer.takeTurn(event, currentGame);
+    computerPlayer.takeTurn(event, currentGame);
+
     fightButton.classList.remove('invisible'); //<-------instead of a hardwired button, make a new one here? or have a fxn invoked taht does that?
     event.target.classList.add('shake'); //how make only one selected? series of conditoinals?
-  }
-  if (currentGame.type === 'classic' && allFighterTypes.includes(event.target.id)) {
-    computerPlayer.generateComputerFighter(classicTypes);
-    currentGame.computerFighter = computerPlayer.fighter;
-  } else if (currentGame.type === 'spooky' && allFighterTypes.includes(event.target.id)) {
-    computerPlayer.generateComputerFighter(spookyTypes);
-    currentGame.computerFighter = computerPlayer.fighter; //necessary?
   }
 }
 
--- player.js
+++ player.js
@@ -4,10 +4,16 @@ class Player {
     // this.wins = 0; //do we need this here? or is it best in game?
     this.fighter = '';
   }
-  takeTurn(event) {
-    this.fighter = event.target.id;
-  }
-  generateComputerFighter(gameTypeArray) {
-    this.fighter = gameTypeArray[createRandomNumber(gameTypeArray.length)];
+  takeTurn(event, currentGame) {
+    if (this.playerType === 'human') {
+      this.fighter = event.target.id;
+      currentGame.humanFighter = this.fighter; //is this bit necessary to be in both Player and Game?
+    } else if (this.playerType === 'computer' && currentGame.type === 'classic') {
+      this.fighter = classicTypes[createRandomNumber(classicTypes.length)];
+      currentGame.computerFighter = this.fighter;
+    } else if (this.playerType === 'computer' && currentGame.type === 'spooky') {
+      this.fighter = spookyTypes[createRandomNumber(spookyTypes.length)];
+      currentGame.computerFighter = this.fighter;
+    }
   }
 }
