b3a790a2954ca9fb25b0020cae15fac1173afb0b
Brett Kuhn <brettkuhn87@gmail.com>
2022-09-26 21:18:32 -0600

    updated determineWinner to shorten, keep same logic

--- game.js
+++ game.js
@@ -8,29 +8,22 @@ class Game {
   }
 
   determineWinner() {
-    if (this.humanPlayer.fighter.type === this.computerPlayer.fighter.type) {
-      this.tie = true;
-      this.winner = {
-        winner: 'none',
-        resultText: 'draw  😐',
-      };
-      return; //necessary?
-    }
-
     for (var i = 0; i < humanWinConditions[currentGame.type].length; i++) {
-      if (humanWinConditions[currentGame.type][i].humanFighter === this.humanPlayer.fighter.type && humanWinConditions[currentGame.type][i].computerFighter.includes(this.computerPlayer.fighter.type)) {
+      if (this.humanPlayer.fighter.type === this.computerPlayer.fighter.type) {
+        this.tie = true;
+        this.winner = {
+          winner: 'none',
+          resultText: 'draw  😐',
+        };
+        return;
+      } else if (humanWinConditions[currentGame.type][i].humanFighter === this.humanPlayer.fighter.type && humanWinConditions[currentGame.type][i].computerFighter.includes(this.computerPlayer.fighter.type)) {
         this.winner = this.humanPlayer;
         this.humanPlayer.wins ++;
-        //could i put the tie in here?????
-        return; //necessary?
+        return;
       }
     }
-
     this.winner = this.computerPlayer;
     this.computerPlayer.wins ++;
-
-//^^^ if we did have the plaers in one key as an array,
-//could we loop through the array and check then have an if then statment??? idk maybe
   }
 
   resetBoard() {
